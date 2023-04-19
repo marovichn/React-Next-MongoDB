634ed25c6675d800126fb815ecf9b5fd63d27fdb
marovichn <nikolamarovic.11@gmail.com>
2023-04-19 23:15:35 +0200

    next-js-getStaticProps

--- project-1/pages/index.js
+++ project-1/pages/index.js
@@ -1,7 +1,32 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://marovichn:<Password>@nikola.wojtt5i.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => {
+        return {
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+          id: meetup._id.toString(),
+        };
+      }),
+    },
+    revalidate: 10,
+  };
+}
 export default HomePage;
